ac6fbb21fac190993573be9af614947241f7754b
Zanhua1999 <skyair886@gmail.com>
2020-02-04 10:58:05 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 04 feb 2020 om 10:57
+-- Serverversie: 10.4.6-MariaDB
+-- PHP-versie: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sterrenstelsel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `planeten`
+--
+
+CREATE TABLE `planeten` (
+  `naam` varchar(50) NOT NULL,
+  `Zon` varchar(50) NOT NULL,
+  `Mercurius` varchar(50) NOT NULL,
+  `Venus` varchar(50) NOT NULL,
+  `Aarde` varchar(50) NOT NULL,
+  `Mars` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
